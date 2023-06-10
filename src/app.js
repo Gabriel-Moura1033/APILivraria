bcceca5257d6af9e5a4b54957a6384663cd7acc2
Gabriel Moura <rez2000@outlook.com>
2023-06-10 01:33:51 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,13 +2,15 @@ import  express  from "express";
 
 const app = express();
 
+app.use(express.json());
+
 const livros = [
     {
-        id: 1,
+        "id": 1,
         "titulo": "Senhor dos aneis"
     },
     {
-        id: 2,
+        "id": 2,
         "titulo": "O Hobbit"
     }
 ];
@@ -21,5 +23,9 @@ app.get('/livros', (req, res) => {
     res.status(200).json(livros);
 });
 
+app.post('/livros', (req, res) => {
+  livros.push(req.body);
+  res.status(201).send("Livro cadastrado com sucesso.");  
+})
 
 export default app;
