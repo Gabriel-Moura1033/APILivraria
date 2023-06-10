999ebf3ab03d75ce23ff88f954a3049af2795fce
Gabriel Moura <rez2000@outlook.com>
2023-06-10 10:58:45 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,9 @@
 import  express  from "express";
 
+function buscaLivro(id) {
+    return livros.findIndex(livro => livro.id == id)
+}
+
 const app = express();
 
 app.use(express.json());
@@ -27,5 +31,13 @@ app.post('/livros', (req, res) => {
   livros.push(req.body);
   res.status(201).send("Livro cadastrado com sucesso.");  
 })
+
+app.put('/livros/:id', (req, res) => {
+    let index = buscaLivro(req.params.id);
+    livros[index].titulo = req.body.titulo;
+    res.status(201).send(`O título do livro ${req.params.id} foi atualizado.`)
+})
+
+
 
 export default app;
