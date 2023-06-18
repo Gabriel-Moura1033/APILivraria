eff34e7d89f67370b96b15104ed15bf16fc60f53
Gabriel Moura <rez2000@outlook.com>
2023-06-18 08:05:04 -0300

    Update livrosController.js

--- src/controllers/livrosController.js
+++ src/controllers/livrosController.js
@@ -2,83 +2,81 @@ import livros from "../models/Livro.js";
 
 class LivroController {
 
-  static listarLivros = (req, res) => {
-    livros.find()
-      .populate("autor")
-      .exec(
-        ((err, livros) => {       
-          res.status(200).json(livros);
-        })
-      );
+  static listarLivros =  async (req, res) => { 
+    try{
+      await livros.find()
+        .populate("autor")
+        .exec(
+          ((erro, livros) => {       
+            res.status(200).json(livros);
+          })
+        );
+    } catch (erro) {
+      res.status(500).json({message: `Ocorreu um erro na busca: ${erro.message}`});
+    }
   };
 
-  static listarLivroPorId = (req, res) => {
-    const id = req.params.id;
-    console.log(id);
-    livros.findById(id)
-      .populate("autor", "nome")
-      .exec(
-        ((err, livros) => {
-          if(err) {
-            res.status(400).send({message: `Livro não encontrado: ${err.message}`});
-          } else {
-            res.status(900).send(livros);
-          }
-        }));
-            
-
-  };
-
-  static cadastrarLivro = (req, res) => {
-    let livro = new livros (req.body);
-    livro.save((err) => {
-      if(err) {
-        res.status(500).send({message: `Falha ao cadastrar o livro: ${err.message}`});
-      } else {
-        res.status(201).send(livro.toJSON());
-      }
-
-
-    });
-  };
-
-  static atualizarLivro = (req, res) => {
+  static listarLivroPorId = async (req, res) => {
     const id = req.params.id;
 
-    livros.findByIdAndUpdate(id, {$set: req.body}, (err) => {
-      if (!err) {
-        res.status(200).send({message: "Livro atualizado com sucesso"});
-      } else {
-        res.status(500).send({message: `Houve um erro ao atualizar o Livro ${err.message}`});
-      }
-    });
+    try {
+      
+      const resultadoLivroId = await livros.findById(id)
+        .populate("autor", "nome")
+        .exec();
+      res.status(200).send(resultadoLivroId);
+      
+
+
+    } catch (erro) {
+      res.status(400).send({message: `Livro não encontrado: ${erro.message}`});
+    }
+
   };
 
-  static excluiLivro = (req, res) => {
+  static cadastrarLivro = async (req, res) => { 
+    try{
+      let livro = new livros (req.body);
+      await livro.save();
+      res.status(201).send(livro.toJSON());
+    } catch (erro) {
+      res.status(500).send({message: `Falha ao cadastrar o livro: ${erro.message}`});
+    }
+  };
+
+  static atualizarLivro = async (req, res) => {
+    try {
+      const id = req.params.id;
+      await livros.findByIdAndUpdate(id, {$set: req.body});
+      res.status(200).send({message: "Livro atualizado com sucesso"});
+
+
+    } catch (erro) {
+      res.status(500).send({message: `Houve um erro ao atualizar o Livro ${erro.message}`});
+    }
+    
+  };
+
+  static excluiLivro = async (req, res) => {
     const id = req.params.id;
+    try {
+      await livros.findByIdAndDelete(id);
+      res.status(200).send({message: "Livro Excluído com sucesso."});
 
-    livros.findByIdAndDelete(id, (err) => {
-      if(!err) {
-        res.status(200).send({message: "Livro Excluído com sucesso."});
-      } else {
-        res.status(500).send({message: `Não foi possível excluir esse livro ${err.message}`});
-      }
-    });
-
-
+    } catch (erro) {
+      res.status(404).send({message: `Não foi possível excluir esse livro ${erro.message}`});
+    }
 
   };
 
-  static listaLivrosEditora = (req, res) => {
+  static listaLivrosEditora = async (req, res) => {
     const editora = req.query.editora;
-
-    livros.find({"editora": editora}, {}, (err, livros) => {
-      if(err) {
-        res.status(400).send({message: "Editora não encontrada"});
-      } else {
-        res.status(200).send(livros);
-      }
-    });
+    try {
+      const resultadoEditoraLivros = await livros.find({"editora": editora}, {});
+      res.status(200).send(resultadoEditoraLivros);
+    } catch (erro) {
+      res.status(400).send({message: "Editora não encontrada"});
+    }
   };
 
 }
